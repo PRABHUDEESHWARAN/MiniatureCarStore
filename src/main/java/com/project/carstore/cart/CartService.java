619ab2c5b1b4e04f924541fc31c2f132f3f3adbe
Nandhini <nxndhiniramesh@gmail.com>
2024-02-21 21:51:17 +0530

    partial implementation

--- src/main/java/com/project/carstore/cart/CartService.java
+++ src/main/java/com/project/carstore/cart/CartService.java
@@ -1,4 +1,15 @@
 package com.project.carstore.cart;
+import java.math.BigDecimal;
+import java.util.List;
+
 
 public interface CartService {
+    public  Cart addItemtoCart(Integer cartId, Integer productId , Integer Quantity);
+    public Cart removeItemFromCart(Integer cartId, Integer cartItemId);
+    public Cart updateItemQuantity(Integer cartItemId, Integer Quantity);
+    public Cart clearCartItems(Integer cartId);
+    List<CartItem> viewCartItems(Integer cartId);
+    BigDecimal calculateTotalCartValue(Integer cartId);
+    boolean checkoutCart(Integer cartId);
+
 }
