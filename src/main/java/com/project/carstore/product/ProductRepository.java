d29296fa2d8e59aa4492a71af3bc8d5a6edbf131
Pooja1829 <85211866+Pooja1829@users.noreply.github.com>
2024-02-23 14:36:18 +0530

    Update ProductRepository.java

--- src/main/java/com/project/carstore/product/ProductRepository.java
+++ src/main/java/com/project/carstore/product/ProductRepository.java
@@ -5,9 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product,Long> {
-    List<Product> getAllProducts();
-
-    Product getProductById(Long id);
 
     List<Product> findByPrice(double price);
 
